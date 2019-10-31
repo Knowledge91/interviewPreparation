755bbb7fbf578eee28520a38b50c19e5195a4631
Dirk Hornung <dirkhornung91@gmail.com>
2019-10-31 14:41:00 +0100

    min stack

---
+++ top_interview_questions_easy/minStack.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stack>
+
+class MinStack {
+private:
+  stack<int> s1;
+  stack<int> s2;
+
+public:
+  void push(int x) {
+    s1.push(x);
+    if (s2.empty() || x <= getMin())
+      s2.push(x);
+  }
+  void pop() {
+    if (s1.top() == getMin())
+      s2.pop();
+    s1.pop();
+  }
+  int top() { return s1.top(); }
+  int getMin() { return s2.top(); }
+};
+
+class Solution {
+public:
+  void test() {}
+};
